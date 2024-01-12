221bda67f898f20c7dd3f2f72bbfe6b16954d421
Elisa Bothy <elisa.bothy@gmail.com>
2024-01-12 10:19:56 +0100

    3e exo, marche enfin !

--- assets/js/g-e.js
+++ assets/js/g-e.js
@@ -1,6 +1,6 @@
 const red = document.getElementById('RED');
 red.addEventListener("click", fonctionResponseRed);
-const blue = document.getElementByID('BLUE');
+const blue = document.getElementById('BLUE');
 blue.addEventListener("click", fonctionResponseBlue);
 
 
